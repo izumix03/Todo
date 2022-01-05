cb80306186f83647fe4e6601c41ef6049fd0e9ea
izumix03 <takahiro.izumikawa@gmail.com>
2022-01-05 18:13:21 +0900

    first commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,33 @@
+//
+//  TodoApp.swift
+//  TodoApp
+//
+//  Created by mix on 2022/01/05.
+//
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    init() {
+        #if DEBUG
+        var injectionBundlePath = "/Applications/InjectionIII.app/Contents/Resources"
+        #if targetEnvironment(macCatalyst)
+        injectionBundlePath = "\(injectionBundlePath)/macOSInjection.bundle"
+        #elseif os(iOS)
+        injectionBundlePath = "\(injectionBundlePath)/iOSInjection.bundle"
+        #endif
+        Bundle(path: injectionBundlePath)?.load()
+        #endif
+    }
+
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
